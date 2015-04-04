bcf9db3e0fbb76fe8c700096e022fe268cf9a619
Asad Shahabuddin <asad.shahabuddin@gmail.com>
2015-04-04 18:00:21 -0400

    Task package

---
+++ src/main/java/com/rasp/task/MapperTask.java
@@ -0,0 +1,15 @@
+/**
+ * Author : Asad Shahabuddin
+ * File   : MapperTask.java
+ * Email  : asad808@ccs.neu.edu
+ * Created: Apr 4, 2015
+ * Edited : Apr 4, 2015
+ */
+
+package com.rasp.task;
+
+public class MapperTask
+{
+	// TODO
+}
+/* End of MapperTask.java */
---
+++ src/main/java/com/rasp/task/TaskTracker.java
@@ -0,0 +1,15 @@
+/**
+ * Author : Pulkit Jain
+ * File   : TaskTracker.java
+ * Email  : 
+ * Created: 
+ * Edited : 
+ */
+
+package com.rasp.task;
+
+public class TaskTracker
+{
+	// TODO
+}
+/* End of TaskTracker.java */
