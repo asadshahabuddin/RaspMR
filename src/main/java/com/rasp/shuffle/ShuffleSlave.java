3bf3d923fd561c3eabbd10a6226fb78ab50529c4
sourabhs <suman.s@husky.neu.edu>
2015-04-12 17:41:31 -0400

    updated method implementations for shuffle slave

--- src/main/java/com/rasp/shuffle/ShuffleSlave.java
+++ src/main/java/com/rasp/shuffle/ShuffleSlave.java
@@ -1,20 +1,23 @@
 package com.rasp.shuffle;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import com.rasp.utils.autodiscovery.Service;
 
 /**
- * Author : rahulmadhavan
+ * Author : Rahul Madhavan, Sourabh Suman
  * File   :
  * Email  : rahulk@ccs.neu.edu
  * Created: 4/9/15
- * Edited :
+ * Edited : 4/12/15
  */
 public interface ShuffleSlave {
 
-    void createDataHandlerFor(String key, Service service);
+    void createDataHandlerFor(String key, Service service) throws FileNotFoundException;
 
-    void storeDataFor(byte[] data, String key, Service service);
+    void storeDataFor(byte[] data, String key, Service service) throws IOException;
 
-    void closeDataHandlerFor(String key, Service service);
+    void closeDataHandlerFor(String key, Service service) throws IOException;
 
 }
--- src/main/java/com/rasp/shuffle/ShuffleSlaveImpl.java
+++ src/main/java/com/rasp/shuffle/ShuffleSlaveImpl.java
@@ -1,5 +1,11 @@
 package com.rasp.shuffle;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
 import com.rasp.config.SlaveConfiguration;
 import com.rasp.utils.autodiscovery.Service;
 
@@ -14,27 +20,51 @@ import com.rasp.utils.autodiscovery.Service;
 public class ShuffleSlaveImpl implements ShuffleSlave {
 
     SlaveConfiguration config;
-
+    Service service;    
+    HashMap<String, FileOutputStream> files;
+    
     public ShuffleSlaveImpl(SlaveConfiguration conf){
         config = conf;
     }
 
     @Override
-    public void createDataHandlerFor(String key, Service service) {
+    public void createDataHandlerFor(String key, Service service) throws FileNotFoundException{
         //create a key K using key and service data
         //create a file for K
+    	String filename = getFileNameFromKeyService(key, service);
+    	File f = new File(filename);    	
+    	
+    	if (!files.containsKey(filename)){
+    		if (f.exists()){
+        		f.delete();
+        	}
+    		files.put(filename, new FileOutputStream(filename, true));    		
+    	}    	    	
     }
 
     @Override
-    public void storeDataFor(byte[] data, String key, Service service) {
+    public void storeDataFor(byte[] data, String key, Service service) throws IOException {
         //create a key K using key and service data
         //fetch the file for K and append data to it
+    	FileOutputStream file = files.get(getFileNameFromKeyService(key, service));
+    	if (file!=null)
+    		file.write(data);
     }
 
     @Override
-    public void closeDataHandlerFor(String key, Service service) {
+    public void closeDataHandlerFor(String key, Service service) throws IOException {
         //create a key using key and service data
         //fetch the file for K and close it
+    	String filename = getFileNameFromKeyService(key, service);
+    	FileOutputStream file = files.get(filename);    	
+    	if (file!=null){
+    		file.close();
+    		files.remove(filename);
+    	}
+    }
+    
+    private String getFileNameFromKeyService(String key, Service service){
+    	return key + "_" + service.getIp().replace(".", "");
     }
 
 }
